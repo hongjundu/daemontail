ed27059303232f838c9bef16020819a405942d2a
duhongjun <hongjun.du.cn@gmail.com>
2020-02-14 09:37:56 +0800

    Init Commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module mytail
+
+go 1.13
+
+require (
+	github.com/hongjundu/go-level-logger v1.0.0
+	github.com/hpcloud/tail v1.0.0
+	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
+	github.com/sevlyar/go-daemon v0.1.5
+	golang.org/x/sys v0.0.0-20200202164722-d101bd2416d5 // indirect
+	gopkg.in/fsnotify.v1 v1.4.7 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"flag"
+	//"fmt"
+	"github.com/hongjundu/go-level-logger"
+	"github.com/hpcloud/tail"
+	"github.com/sevlyar/go-daemon"
+	"log"
+	"os"
+	//"os/signal"
+	"syscall"
+	"time"
+)
+
+var (
+	signal = flag.String("s", "", `Send signal to the daemon:
+  quit — graceful shutdown
+  stop — fast shutdown
+  reload — reloading the configuration file`)
+
+	stop = make(chan struct{})
+	done = make(chan struct{})
+)
+
+func main() {
+	flag.Parse()
+	daemon.AddCommand(daemon.StringFlag(signal, "quit"), syscall.SIGQUIT, termHandler)
+	daemon.AddCommand(daemon.StringFlag(signal, "stop"), syscall.SIGTERM, termHandler)
+	daemon.AddCommand(daemon.StringFlag(signal, "reload"), syscall.SIGHUP, reloadHandler)
+
+	logger.Init(0, "mytail", "/tmp/", 100, 3, 30)
+	logger.Debugf("[main] enters args: %v", os.Args)
+
+	cntxt := &daemon.Context{
+		PidFileName: "/tmp/mytail.pid",
+		PidFilePerm: 0644,
+		LogFileName: "/tmp/mytail2.log",
+		LogFilePerm: 0640,
+		WorkDir:     "./",
+		Umask:       027,
+		Args:        nil,
+	}
+
+	if len(daemon.ActiveFlags()) > 0 {
+		d, err := cntxt.Search()
+		if err != nil {
+			logger.Fatalf("Unable send signal to the daemon: %s", err.Error())
+		}
+		daemon.SendCommands(d)
+		return
+	}
+
+	d, err := cntxt.Reborn()
+	if err != nil {
+		logger.Fatal("Unable to run: ", err)
+	}
+	if d != nil {
+		return
+	}
+	defer cntxt.Release()
+
+	go func() {
+		t, err := tail.TailFile("/tmp/mytail.log", tail.Config{Follow: true})
+
+		if err != nil {
+			logger.Errorln(err)
+			return
+		}
+
+		for line := range t.Lines {
+			log.Println("tail reading: >>> " + line.Text)
+		}
+	}()
+
+	go func() {
+		i := 1
+	LOOP:
+		for {
+			select {
+			case <-stop:
+				break LOOP
+			default:
+			}
+
+			logger.Infof("log line : %d", i)
+			time.Sleep(time.Second)
+			i++
+		}
+
+		done <- struct{}{}
+	}()
+
+	err = daemon.ServeSignals()
+	if err != nil {
+		logger.Errorf("Error: %s", err.Error())
+	}
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	//quit := make(chan os.Signal)
+
+	// kill (no param) default send syscanll.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall. SIGKILL but can"t be catch, so don't need add it
+	//signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	//<-quit
+
+	logger.Infof("Server exited")
+
+}
+
+func termHandler(sig os.Signal) error {
+	logger.Infoln("terminating...")
+	stop <- struct{}{}
+	if sig == syscall.SIGQUIT {
+		<-done
+	}
+	return daemon.ErrStop
+}
+
+func reloadHandler(sig os.Signal) error {
+	logger.Infoln("configuration reloaded")
+	return nil
+}
